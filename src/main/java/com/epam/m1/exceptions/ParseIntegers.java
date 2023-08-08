73ec767cde7c4665854e1170dc0143398ab05934
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-08 16:41:37 +0600

    Stage-1 Task 1

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -9,21 +9,38 @@ import java.util.List;
  * space delimiter
  */
 public class ParseIntegers {
-
+    /*
+     * There is a sentence with words and numbers.
+     * The sentence splitted(" ") to the array of Strings which is called WORDS.
+     * Complete the code, parse integers, calculate the sum of numbers in the WORDS,
+     * also join strings with space delimiter
+     */
     private static final List<String> WORDS =
             Arrays.asList(
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
 
     public static void main(String[] args) {
+
         Iterator<String> words = WORDS.iterator();
+
         int sum = 0;
+
         String justWords = "";
+
+
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
+            try {
+                int number = Integer.parseInt(next); // an exception occurred here (Stack Trace)}
+                sum = sum + number;
+            } catch (NumberFormatException e) {
+                justWords = (justWords + " " + next + " ").strip();
+                justWords = " " + justWords;
+            }
             // todo: complete it
         }
+
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
     }
